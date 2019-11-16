2a0fae62c94465774850ef4fef9e7cab453a1c6f
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-11-16 08:58:26 +0700

    normal vs KTX

--- app/src/main/java/com/gzeinnumer/kaderunandhandler/MainActivity.kt
+++ app/src/main/java/com/gzeinnumer/kaderunandhandler/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gzeinnumer.kaderunandhandler
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,8 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mRandom: Random
     private lateinit var mHandler: Handler
-    private lateinit var mRunnable:Runnable
+    private lateinit var mRunnable: Runnable
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,49 +24,37 @@ class MainActivity : AppCompatActivity() {
         mRandom = Random()
         mHandler = Handler()
 
-        button_run.setOnClickListener{
-            mRunnable = Runnable {
-                // Do something here
-                root_layout.setBackgroundColor(randomHSVColor())
-                text_view.text = "Handler Example\n" +
-                        "Random Number : ${mRandom.nextInt(100)}"
+        mRunnable = Runnable {
+            // Do something here
+            root_layout.setBackgroundColor(randomHSVColor())
+            text_view.text = "Handler Example\n" +
+                    "Random Number : ${mRandom.nextInt(100)}"
 
-                //NORMAL
-                mHandler.postDelayed(mRunnable, 1000)
-                //KTX
-                mHandler.postDelayed(delayInMillis = 1000){ mRunnable }
-
-            }
-
+            //start handler
             //NORMAL
-            mHandler.postDelayed(mRunnable,1000 )
+            mHandler.postDelayed(mRunnable, 1000)
             //KTX
-            mHandler.postDelayed(delayInMillis  = 3000){ mRunnable }
+            mHandler.postDelayed(delayInMillis = 1000) { mRunnable }
         }
 
-        button_stop.setOnClickListener{
-            mHandler.removeCallbacks(mRunnable)
-            text_view.text = "Handler call backs removed."
+        //NORMAL
+        mHandler.postDelayed({
+            text_view.text = "Handler Short Code\n" +
+                    "Random Number : ${mRandom.nextInt(100)}"
+            text_view.setTextColor(randomHSVColor())
+            root_layout.setBackgroundColor(Color.WHITE)
+        }, 3000) // 3 seconds delay task execution
+
+        //KTX
+        mHandler.postDelayed(delayInMillis = 3000) {
+            text_view.text = "Handler Short Code\n" +
+                    "Random Number : ${mRandom.nextInt(100)}"
+            text_view.setTextColor(randomHSVColor())
+            root_layout.setBackgroundColor(Color.WHITE)
         }
 
-        button_short.setOnClickListener{
-            //NORMAL
-            mHandler.postDelayed({
-                text_view.text = "Handler Short Code\n" +
-                        "Random Number : ${mRandom.nextInt(100)}"
-                text_view.setTextColor(randomHSVColor())
-                root_layout.setBackgroundColor(Color.WHITE)
-            }, 3000) // 3 seconds delay task execution
-
-            //KTX
-            mHandler.postDelayed(delayInMillis  = 3000){
-                text_view.text = "Handler Short Code\n" +
-                        "Random Number : ${mRandom.nextInt(100)}"
-                text_view.setTextColor(randomHSVColor())
-                root_layout.setBackgroundColor(Color.WHITE)
-            }
-
-        }
+        //stop run
+        mHandler.removeCallbacks(mRunnable)
     }
 
     fun randomHSVColor(): Int {
